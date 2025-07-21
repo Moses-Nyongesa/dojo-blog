e9e7c4fa74597073f0f74b86ca3af648e9d8ae3b
Moses-Nyongesa <nyongesamoses018@gmail.com>
2025-07-21 08:34:56 +0300

    Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -3,20 +3,24 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
+useEffect(() => {
+    setTimeout(() => {
         fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            console.log(data);
-            setBlogs(data);
-        })
-    }, []);
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsLoading(false);
+            });
+    }, 1000);
+}, []);
 
     return (
         <div className="home">
+         {isLoading && <div>Loading...</div>}
         {blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
         </div>
      );
